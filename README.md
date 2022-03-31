01e3736bf9fef3804a8d81a4283a7a9f246caa3d
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-03-31 16:19:58 +0530

    feat: create a basic CLI for use to jiggle the mouse

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-#
+# jiggle - A Tiny Program to Jiggle Your Mouse
+
+Tired of keeping your laptop unlocked while you do your boring job, where you have nothing to do?
+Tired of your manager telling you that you went "away" for far too long?
+Worry no longer! `jiggle` is here to empower you to do other things when you're "working" from
+home!
+
+*Jokes apart*, jiggle is a tiny program to help you keep your laptop awake when you don't want the
+screen to suspend. This is very useful when you're keeping an eye on a dashboard perhaps, or you
+want to somehow keep your system unlocked for some reason.
+
+## Usage
+
+Run `jiggle` to run this command every 10 seconds. If you'd like to adjust the time setting, you
+can use `jiggle <SECONDS>`, such as `jiggle 60` to run the command every 60s.
+
+What Jiggle does is it moves your mouse a *tiny* bit every few seconds. That's enough to keep your
+computer from sleeping.
+
+## Installation
+
+Download the binary from the releases, and run the command whenever you want.
+
+Note that you should install `libxdo-dev`, `xdotool`, or `libxdo-devel` if you're trying to build
+this using `cargo` on Linux.
+
+This should work on MacOs, Windows and Linux.
+
+Raise issues [here](https://github.com/stonecharioteer/wfh-jiggle).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use log::debug;
+use std::{env, thread, time::Duration};
+use structopt::StructOpt;
+
+use mouse_rs::{types::Point, Mouse};
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "jiggle",
+    about = "A tiny program that jiggles your mouse every \
+    few seconds so that your computer doesn't get locked.\
+    Use `RUST_LOG=debug` to output logs."
+)]
+struct Opt {
+    /// Seconds to wait before moving your mouse.
+    /// default: 10s, can also be set by using the `JIGGLE_SLEEP`
+    /// environment variable.
+    seconds: Option<u64>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Get the current mouse location.
+    let opt = Opt::from_args();
+    env_logger::init();
+    debug!("Jiggle started.");
+    let mouse_controller = Mouse::new();
+    let sleep_duration = match opt.seconds {
+        Some(v) => v,
+        None => {
+            let sleep_duration_result = env::var("JIGGLE_SLEEP");
+            let default_sleep = 10; // 10 seconds
+            let val: u64 = match sleep_duration_result {
+                Ok(v) => match v.to_string().parse::<u64>() {
+                    Ok(x) => x,
+                    Err(_) => default_sleep,
+                },
+                Err(_) => default_sleep,
+            };
+            val
+        }
+    };
+    debug!("Sleep duration is set to {sleep_duration}s.");
+    loop {
+        let old_position = mouse_controller.get_position().unwrap();
+        let new_position = Point {
+            x: old_position.x + 1,
+            y: old_position.y + 1,
+        };
+        debug!("Moving the mouse a tiny bit.");
+        _ = mouse_controller.move_to(new_position.x, new_position.y);
+        _ = mouse_controller.move_to(old_position.x, old_position.y);
+        debug!("Sleeping for {sleep_duration}s.");
+        thread::sleep(Duration::new(sleep_duration, 0));
+    }
 }
