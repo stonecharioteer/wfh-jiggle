259c12d63b5022bec9414a305d6b8715b4124317
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-04-01 12:23:25 +0800

    Refactor: prefer `NonZeroU64` over plain `u64`
    This is mainly for the sake of optimizing the memory layout.
    Also, it doesn't make sense to provide zero delay, anyway.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use log::debug;
-use std::{env, thread, time::Duration};
+use std::{env, num::NonZeroU64, thread, time::Duration};
 use structopt::StructOpt;
 
 use mouse_rs::{types::Point, Mouse};
@@ -15,7 +15,7 @@ struct Opt {
     /// Seconds to wait before moving your mouse.
     /// default: 10s, can also be set by using the `JIGGLE_SLEEP`
     /// environment variable.
-    seconds: Option<u64>,
+    seconds: Option<NonZeroU64>,
 }
 
 fn main() {
@@ -23,13 +23,19 @@ fn main() {
     let opt = Opt::from_args();
     env_logger::init();
     debug!("Jiggle started.");
+
     let mouse_controller = Mouse::new();
-    let sleep_duration = opt.seconds.unwrap_or_else(|| {
-        env::var("JIGGLE_SLEEP")
-            .ok()
-            .and_then(|var| var.parse().ok())
-            .unwrap_or(10) // 10 seconds...
-    });
+    let sleep_duration = opt
+        .seconds
+        .or_else(|| {
+            env::var("JIGGLE_SLEEP")
+                .ok()
+                .and_then(|var| var.parse().ok())
+                .and_then(NonZeroU64::new)
+        })
+        .map(NonZeroU64::get)
+        .unwrap_or(10);
+
     debug!("Sleep duration is set to {sleep_duration}s.");
     loop {
         let old_position = mouse_controller.get_position().unwrap();
